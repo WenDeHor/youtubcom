7f7432d4a389736747deb9ca080d76097fdda4dd
WenDeHor <gaheru20>
2020-07-12 19:58:13 +0300

    1.Min comp for study at web

---
+++ src/main/java/com/studio/youtubcom/YoutubcomApplication.java
@@ -0,0 +1,14 @@
+package com.studio.youtubcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubcomApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(YoutubcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studio/youtubcom/YoutubcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.youtubcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
